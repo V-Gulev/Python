a2960629436b889234525ce66171e42716b412fc
V-Gulev <velizargulev@abv.bg>
2024-11-20 19:32:57 +0200

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,14 @@
-x = int(input("x: "))
-y = int(input("y: "))
-z = x / y
-print(f"{z:.50f}")
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
+
+def main():
+    x = get_int("x: ")
+    y = get_int("y: ")
+
+    print(x + y)
+
+main()
